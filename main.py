aa44027b1524b3f0ad0fb722ae2760fab609594b
venturk <33842147+venturk@users.noreply.github.com>
2020-05-11 15:47:15 +0300

    Finished with the short functions (i.e print_board, possible_moves & is_board_full)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def print_board(board):
-    pass
+def print_board(board):  # Board is represented as a single list
+    for i in range(3):
+        for j in range(3):
+            print(board[3*i + j]+" |", end=' ')
+        print("")
 
 
 def has_won(board, shape):  # Check rows, diagonals and columns
@@ -7,11 +10,11 @@ def has_won(board, shape):  # Check rows, diagonals and columns
 
 
 def possible_moves(board):  # each empty space is a possible move
-    pass
+    return [i for i, x in enumerate(board) if x == ' ']
 
 
 def is_board_full(board):  # Board is full if there is no empty space left
-    pass
+    return ' ' not in board
 
 
 # If board is full AND no player won, there is a TIE...
