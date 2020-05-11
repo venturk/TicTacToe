84e309cf643d09c26ccae6b306f868c6272ca309
venturk <33842147+venturk@users.noreply.github.com>
2020-05-11 15:40:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+def print_board(board):
+    pass
+
+
+def has_won(board, shape):  # Check rows, diagonals and columns
+    pass
+
+
+def possible_moves(board):  # each empty space is a possible move
+    pass
+
+
+def is_board_full(board):  # Board is full if there is no empty space left
+    pass
+
+
+# If board is full AND no player won, there is a TIE...
+# Is there a need for this function...?
+def is_tie(board):
+    pass
+
+
+# Should check for validity?
+# Or is it already being checked?
+# (Note to self - Make sure you don't make wasteful work)
+def make_move(board, position, shape):
+    pass
+
+
+def minimax(board, isMaximizing):  # The well known AI minimax algorithm
+    pass
