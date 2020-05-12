6ff0be1e8b0dd1d553ee90c0bbb19445c4b80103
venturk <ventura.kfir@gmail.com>
2020-05-12 19:21:34 +0300

    GUI is updated with basic game flow. All is working, need to organize the code.

--- GUI.py
+++ GUI.py
@@ -1,8 +1,8 @@
+from main import print_board, is_board_full, has_won, ai_player
+import pygame
 import sys
 
-import pygame
-
-WINDOW_WIDTH = WINDOW_HEIGHT = 900
+WINDOW_WIDTH = WINDOW_HEIGHT = 1050
 BLOCK_SIZE = WINDOW_WIDTH // 3  # We want 3 cells in each row and each column
 
 
@@ -29,7 +29,7 @@ def get_cell_pos_from_cords(cords):
 
 
 def get_shape_pos(row, col):
-    x, y = 25, 6
+    x, y = BLOCK_SIZE // 3, BLOCK_SIZE // 6
 
     x += col * BLOCK_SIZE
     y += row * BLOCK_SIZE
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     pygame.init()
     board = [' '] * 9  # Init board with 9 cells
     pygame.display.set_caption('Tic Tac Toe')  # Set window title
-
+    font = pygame.font.SysFont('Arial', BLOCK_SIZE // 2)  # Set font
     window = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
     window.fill((0, 0, 0))
     draw_grid(window)
@@ -52,5 +52,31 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:  # Quit
                 pygame.quit()
                 sys.exit()
+
+            if not game_over and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left mouse button click
+                coordinates = pygame.mouse.get_pos()
+                row, column = get_cell_pos_from_cords(coordinates)
+
+                if board[3 * row + column] != ' ':  # If spot is unavailable, DO NOTHING
+                    continue
+
+                x, y = get_shape_pos(row, column)
+
+                board[3 * row + column] = 'X'
+                print_board(board)
+                window.blit(font.render('X', True, (255, 0, 0)), (x, y))
+                # current_player = 'X' if current_player == 'O' else 'O'
+
+                ai_move = ai_player(board)
+                if ai_move is not None:  # Happens only if board is full
+                    x, y = get_shape_pos(ai_move // 3, ai_move % 3)
+                    board[ai_move] = 'O'
+                    window.blit(font.render('O', True, (255, 0, 0)), (x, y))
+
+                if has_won(board, 'O'):
+                    game_over = True
+
+                if is_board_full(board):
+                    game_over = True
 
         pygame.display.update()
